d330d1df7489b5d41f1efeae032c96f3fd976eb7
Webmantras <development.help24@gmail.com>
2015-07-19 02:59:07 +0530

    Update 0001_initial.py

--- ecommerce/accounts/migrations/0001_initial.py
+++ ecommerce/accounts/migrations/0001_initial.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models, migrations
-from django.conf import settings
-
-
+# -*
 class Migration(migrations.Migration):
 
     dependencies = [
